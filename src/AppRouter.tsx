4f5f9a8bfc79d847a5cd62721763f3b03d2c457e
Creativedelight <creativedelights809@gmail.com>
2025-09-28 04:23:20 -0700

    updated AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { EventDetailPage } from "./pages/EventDetailPage";
 import { TicketPurchasePage } from './pages/TicketPurchasePage';
 import { ConfirmationPage } from './pages/ConfirmationPage';
 import { UserDashboardPage } from './pages/UserDashboardPage';
@@ -10,6 +11,7 @@ export function AppRouter() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="events/:eventSlug" element={<EventDetailsPage />} />
           <Route path="purchase/:eventId" element={<TicketPurchasePage />} />
           <Route path="confirmation/:ticketId" element={<ConfirmationPage />} />
           <Route path="my-tickets" element={<UserDashboardPage />} />
